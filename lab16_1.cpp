2046d623c640a2f2f891fb187f12e341e8a467d2
PatcharapaChainet <patcharapa_chainet@cmu.ac.th>
2025-01-31 14:27:44 +0700

    lab16 doneee

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,38 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double *x,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << x[i];
+        if((i+1)%M == 0 ) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *x,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        x[i] = (rand()%101)/100.0;
+    }
+}
+
+void findRowSum(const double *a, double *b, int N, int M) {
+    for (int i = 0; i < N; i++) {
+        b[i] = 0.0;
+        for (int j = 0; j < M; j++) {
+            b[i] += *(a + i * M + j);
+        }
+    }
+}
+
+void findColSum(const double *a, double *b, int N, int M) {
+	for (int i = 0; i < M; i++) {
+        b[i] = 0.0;
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            b[j] += a[i * M + j];
+        }
+    }
+}
